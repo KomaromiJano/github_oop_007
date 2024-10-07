bdcf6e7dd1336a17e39a7a38556868ffeb41e7e7
Komáromi János <komaromijano@gmail.com>
2024-10-07 09:14:17 +0200

    Call network functions and add new variable network.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Network network = new Network();
+        network.connectToNetwork("Kulker");
+        network.disconnectFromNetwork();
     }
 }
--- src/Network.java
+++ src/Network.java
@@ -1,13 +1,16 @@
 public class Network implements Connectable {
 
+    private String networkName;
+
     @Override
     public void connectToNetwork(String networkName) {
-        System.out.println("Csatlakozva a hálozathoz.");
+        this.networkName = networkName;
+        System.out.println("Csatlakozva a " + networkName + " nevű hálozathoz.");
     }
 
     @Override
     public void disconnectFromNetwork() {
-        System.out.println("Lecsatlakozva a hálozatról.");
+        System.out.println("Lecsatlakozva a " + this.networkName + " nevű hálozatról.");
     }
 
 }
